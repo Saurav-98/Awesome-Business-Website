1c319c2335bd7755d60cb297ee345f820d63b36f
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-23 02:38:04 +0530

    CardDeal, Testimonials Section Completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles}`}
+      className={`rounded-[10px] font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles}`}
     >
       Get Started
     </button>
